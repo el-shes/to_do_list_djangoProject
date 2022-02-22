417415b67dcf59b53cddbc03b95f71a2d3605df5
rrfpCSIR <shes980@gmail.com>
2022-02-22 16:03:01 +0200

    initial task rendering

--- list_app/models.py
+++ list_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    task_title = models.CharField(max_length=200)
+    task_description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_title
+
+    class Meta:
+        ordering = ["complete"]
