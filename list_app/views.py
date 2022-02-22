3f27298c8c08ede7b4d356f71fe5947d9fcc5eff
rrfpCSIR <shes980@gmail.com>
2022-02-22 16:47:53 +0200

    added detail task view

--- list_app/views.py
+++ list_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from list_app.models import Task
+
+
 # Create your views here.
 
 
 class TaskList(ListView):
+    model = Task
+
+
+class TaskDetail(DetailView):
     model = Task
