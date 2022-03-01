3ded626361de9fbabdcbc9489b8599e57c0a4915
rrfpCSIR <shes980@gmail.com>
2022-03-01 16:20:10 +0200

    Register form is added

--- list_app/views.py
+++ list_app/views.py
@@ -1,12 +1,15 @@
-
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from list_app.models import Task
 
@@ -21,6 +24,26 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'list_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        # create and redirect user to a tasks page
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        # if the user is logged in - request to login page is redirected to the tasks page
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
