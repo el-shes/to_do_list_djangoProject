564369fbbcdf5e04f8519091f278d5d8d3abfb4d
rrfpCSIR <shes980@gmail.com>
2022-03-01 20:45:30 +0200

    Search engine added

--- list_app/views.py
+++ list_app/views.py
@@ -54,6 +54,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        # adding search button logic
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(task_title__startswith=search_input)
+
+        context['search_input'] = search_input
         return context
 
 
